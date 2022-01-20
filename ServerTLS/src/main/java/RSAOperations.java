d2b22d5c217c64db9b5c6fa2672e35bdaecc2cf0
DanielCorreia21 <danielcorreia210100@gmail.com>
2022-01-20 22:57:56 +0000

    Added operations to allow digital signatures

---
+++ ServerTLS/src/main/java/RSAOperations.java
@@ -0,0 +1,89 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+
+public class RSAOperations {
+
+//	public static void main(String[] args) throws Exception {
+//		
+//		PrivateKey privKey = getPrivateKeyFromFile("Keys/server.key");
+//		Certificate cert = getCertificate("Keys/server.crt");
+//		PublicKey pubKey = cert.getPublicKey();
+//		String msg = "Hello, I am someone";
+//		String signature = sign(privKey, msg);
+//		System.out.println(verify(pubKey, msg, signature));
+//
+//		Certificate cert2 = getCertificate("Keys/rootCA.crt");
+//		PublicKey pubKey2 = cert2.getPublicKey();
+//		String signature2 = sign(privKey, msg + "a");
+//		System.out.println(verify(pubKey, msg, signature2));
+//		System.out.println(verify(pubKey2, msg, signature));
+//	}
+
+	public static X509Certificate getCertificate(String certificatePath) throws Exception {
+		CertificateFactory fac = CertificateFactory.getInstance("X509");
+		FileInputStream is = new FileInputStream(certificatePath);
+		X509Certificate cert = (X509Certificate) fac.generateCertificate(is);
+		//TODO check validity of certificate?
+		return cert;
+	}
+	
+	public static RSAPrivateKey getPrivateKeyFromFile(String filename) throws IOException, GeneralSecurityException {
+	    String privateKeyPEM = new String(Files.readAllBytes(Paths.get(filename)), Charset.defaultCharset());
+	    privateKeyPEM = privateKeyPEM.replace("-----BEGIN PRIVATE KEY-----\n", "");
+	    privateKeyPEM = privateKeyPEM.replace("-----END PRIVATE KEY-----", "");
+	    byte[] encoded = Base64.getMimeDecoder().decode((privateKeyPEM));
+	    KeyFactory kf = KeyFactory.getInstance("RSA");
+	    PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+	    RSAPrivateKey privKey = (RSAPrivateKey) kf.generatePrivate(keySpec);
+	    return privKey;
+	}
+	
+	public static String sign(PrivateKey privateKey, String message) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
+	    Signature sign = Signature.getInstance("SHA256withRSA");
+	    sign.initSign(privateKey);
+	    sign.update(message.getBytes("UTF-8"));
+	    return new String(Base64.getEncoder().encodeToString((sign.sign())));
+	}
+
+
+	public static boolean verify(PublicKey publicKey, String message, String signature) throws SignatureException, NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
+	    Signature sign = Signature.getInstance("SHA256withRSA");
+	    sign.initVerify(publicKey);
+	    sign.update(message.getBytes("UTF-8"));
+	    return sign.verify(Base64.getMimeDecoder().decode((signature.getBytes("UTF-8"))));
+	}
+
+//	public static String encrypt(String rawText, PublicKey publicKey) throws IOException, GeneralSecurityException {
+//	    Cipher cipher = Cipher.getInstance("RSA");
+//	    cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+//	    return Base64.getMimeEncoder().encodeToString((cipher.doFinal(rawText.getBytes("UTF-8"))));
+//	}
+//
+//	public static String decrypt(String cipherText, PrivateKey privateKey) throws IOException, GeneralSecurityException {
+//	    Cipher cipher = Cipher.getInstance("RSA");
+//	    cipher.init(Cipher.DECRYPT_MODE, privateKey);
+//	    return new String(cipher.doFinal(Base64.getMimeDecoder().decode((cipherText))), "UTF-8");
+//	}
+
+
+}
